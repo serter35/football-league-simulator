794b2cbedb143bad281e01a3db03b6fa2ccc8cf3
serter <serter.serbest@gmail.com>
2026-02-06 20:53:46 +0300

    Routes fix

--- routes/web.php
+++ routes/web.php
@@ -3,15 +3,9 @@
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\LeagueController;
 
-// Hem Basic Auth hem de Hız Sınırlandırma (Dakikada 60 istek)
 Route::middleware(['auth.basic', 'throttle:60,1'])->group(function () {
-
-    // Ana Sayfa
-    Route::get('/', [LeagueController::class, 'index'])->name('league.index');
-
-    // İşlem Rotaları (Post işlemleri throttle için daha kritiktir)
-    Route::post('/simulate-week', [LeagueController::class, 'simulateWeek']);
-    Route::post('/simulate-all', [LeagueController::class, 'simulateAll']);
-    Route::post('/reset', [LeagueController::class, 'reset']);
-    Route::post('/update-scores', [LeagueController::class, 'updateScores']);
+    Route::get('/', [LeagueController::class, 'index'])->name('index');
+    Route::post('/generate', [LeagueController::class, 'generate'])->name('generate');
+    Route::post('/simulate-week/{week}', [LeagueController::class, 'simulateNextWeek'])->name('simulate.week');
+    Route::post('/simulate-all', [LeagueController::class, 'simulateAll'])->name('simulate.all');
 });
